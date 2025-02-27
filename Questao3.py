691c9b65a1a5da2759dffe5e69891414d3c601c8
brunocoelhosi <brunocoelho.si@outlook.com>
2025-02-27 10:38:03 -0300

    exercicios

--- Questao3.py
+++ Questao3.py
@@ -5,25 +5,26 @@ with open("dados.json", "r", encoding="utf-8") as arquivo:
     dados = json.load(arquivo) #Json para dicionario
 
 #Tratamneto dos dados
-#Menor valor
+#O menor valor de faturamento ocorrido em um dia do mês;
 for i in range(len(dados)):
     menor = 0
     if dados[i]["valor"] < dados[menor]["valor"]:
         menor = i
 print(f"Menor Valor: {dados[menor]["valor"]} - Dia: {dados[menor]["dia"]}")
 
-#Maior valor
+#O maior valor de faturamento ocorrido em um dia do mês;
 for i in range(len(dados)):
     maior = 0
     if dados[i]["valor"] > dados[maior]["valor"]:
         maior = i
 print(f"Maior Valor: {dados[maior]["valor"]} - Dia: {dados[maior]["dia"]}")
 
-#Media
-for i in range(len(dados)):
-    media = 0
-    c = 1
-    if dados[i]["valor"] > 0:
-        media = media + dados[i]["valor"]
-        c+=1
-print(f"Média dos valores: {media/c}")
+#Calculando a media do faturamento
+faturamento_valido = [dia for dia in dados if dia["valor"] > 0] #Ignorando os valores 0
+soma_faturamento = sum(dia["valor"] for dia in faturamento_valido)
+media_faturamento = soma_faturamento / len(faturamento_valido)
+print(f"Media do faturamento: {media_faturamento:.2f}")
+
+# Contando os dias acima da média
+dias_acima_da_media = sum(1 for dia in faturamento_valido if dia["valor"] > media_faturamento)
+print(f"Dias com faturamento acima da média: {dias_acima_da_media}")
